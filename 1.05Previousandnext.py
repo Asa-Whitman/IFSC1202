7e6c87cbf32cb1ad4e2dad983a9bcb4335106543
Asa-Whitman <acwhitman@ualr.edu>
2022-08-31 15:48:28 +0000

    previous and next number

---
+++ 1.05Previousandnext.py
@@ -0,0 +1,6 @@
+Base = int(input("Enter number:"))
+Nextnumber = Base + 1
+Previousnumber = Base - 1
+
+print("The number after " + str(Base) + " is: " + str(Nextnumber))
+print("The number before " + str(Base) + " is: " + str(Previousnumber))
