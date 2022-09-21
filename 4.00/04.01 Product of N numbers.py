7b1738b11a63f37be5ced0ef0cb49c326be877fc
Asa-Whitman <acwhitman@ualr.edu>
2022-09-21 15:51:30 +0000

    Product of N numbers

---
+++ 4.00/04.01 Product of N numbers.py
@@ -0,0 +1,9 @@
+#coding by Asa Whitman
+import math
+N = int(input("how many numbers will be multiplied? "))
+final = 1
+int(final)
+for i in range(N):
+    mult=int(input("enter a number: "))
+    final *= mult
+print(final)
